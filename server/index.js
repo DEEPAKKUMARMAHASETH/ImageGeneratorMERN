091276ff9e4bc0c6a8c1d4054e7080b7fceaea88
deepak <deepak@right2vote.in>
2024-08-10 14:41:54 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ import GenerateImageRouter from "./routes/GenerateImage.js";
 dotenv.config();
 const PORT = process.env.PORT || 8080
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://image-generator-mern.vercel.app"
+  ],
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
