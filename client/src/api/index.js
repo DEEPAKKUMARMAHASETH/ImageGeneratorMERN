264c8769681e4c37dbab668af8220f40d91d0522
deepak <deepak@right2vote.in>
2024-08-10 22:45:57 +0530

    api change

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://image-generator-mern-backend.vercel.app/api/",
+  baseURL: "https://image-generator-mern-backend.vercel.app/api/",
   // baseURL: "https://image-generator-4l8k.onrender.com/api/",
-  baseURL:'http://localhost:8080/api/'
+  // baseURL:'http://localhost:8080/api/'
 });
 
 export const GetPosts = async () => await API.get("/post/");
