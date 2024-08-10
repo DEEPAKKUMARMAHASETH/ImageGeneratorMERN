5b7945b06553d09c31775fe20d8c5f56e4ee4c9b
deepak <deepak@right2vote.in>
2024-08-10 21:10:33 +0530

    allowing cors for all

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://image-generator-mern.vercel.app"
-  ],
+  origin: '*',
 }));
 //dummy commit
 app.use(express.json({ limit: "50mb" }));
