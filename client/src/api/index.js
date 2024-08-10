02e29dd3ebfcc606b57ec7022b8d3b06423ca263
deepak <deepak@right2vote.in>
2024-08-10 23:28:46 +0530

    fghjk

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,7 +5,7 @@ const API = axios.create({
   // baseURL: "https://image-generator-4l8k.onrender.com/api/",
   // baseURL:'http://localhost:8080/api/'
 });
-
+//rghj
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
 export const GenerateAIImage = async (data) =>
